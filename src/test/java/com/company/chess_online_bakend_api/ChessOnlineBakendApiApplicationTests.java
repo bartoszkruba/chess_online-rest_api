19c45096b9bf18012eadb28645c79813827f2786
bartoszkruba <bartosz.kruba@gmail.com>
2019-06-15 18:14:02 +0200

    Initialized project. Closes #1

---
+++ src/test/java/com/company/chess_online_bakend_api/ChessOnlineBakendApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.chess_online_bakend_api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChessOnlineBakendApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
