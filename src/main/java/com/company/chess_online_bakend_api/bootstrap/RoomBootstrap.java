42a343f9a96204bbf0d940bd516d78070b69764c
bartoszkruba <bartosz.kruba@gmail.com>
2019-07-08 16:29:42 +0200

    Added newGame util method to Room + tests. #23

---
+++ src/main/java/com/company/chess_online_bakend_api/bootstrap/RoomBootstrap.java
@@ -0,0 +1,16 @@
+package com.company.chess_online_bakend_api.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class RoomBootstrap implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadRooms();
+    }
+
+    private void loadRooms() {
+
+    }
+
+}
--- src/main/java/com/company/chess_online_bakend_api/data/model/Room.java
+++ src/main/java/com/company/chess_online_bakend_api/data/model/Room.java
@@ -1,12 +1,10 @@
 package com.company.chess_online_bakend_api.data.model;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @EqualsAndHashCode(callSuper = true)
@@ -16,13 +14,48 @@ import java.time.LocalDateTime;
 public class Room extends BaseEntity {
 
     @OneToOne
-    Game game;
+    @Setter(AccessLevel.NONE)
+    private Game game;
 
-    String name;
+    private String name;
 
     @Builder
     public Room(Long id, LocalDateTime created, LocalDateTime updated, String name) {
         super(id, created, updated);
         this.name = name;
+
+        startNewGame();
+    }
+
+    public Game startNewGame() {
+
+        Game game = Game.builder()
+                .status(GameStatus.WAITNG_TO_START)
+                .turn(0)
+                .room(this)
+                .build();
+        this.game = game;
+
+        // TODO: 2019-07-08 Consider creating archive microservice / RabbitMQ
+        // TODO: 2019-07-08 move old game to archive
+
+        return game;
+    }
+
+    public Game startNewGame(@NotNull User whitePlayer, @NotNull User blackPlayer) {
+
+        if (whitePlayer == null || blackPlayer == null) {
+            throw new NullPointerException();
+        }
+
+        Game game = Game.builder().status(GameStatus.STARTED)
+                .turn(1)
+                .whitePlayer(whitePlayer)
+                .blackPlayer(blackPlayer)
+                .room(this).build();
+
+        this.game = game;
+
+        return game;
     }
 }
