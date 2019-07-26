ee98a7c3e53855a05d62cb305ce21524ccad07a5
bartoszkruba <bartosz.kruba@gmail.com>
2019-07-26 20:23:17 +0200

    Implemented message page REST path + tests. #25.

--- src/main/java/com/company/chess_online_bakend_api/controller/ChatMessageController.java
+++ src/main/java/com/company/chess_online_bakend_api/controller/ChatMessageController.java
@@ -8,6 +8,7 @@
 
 package com.company.chess_online_bakend_api.controller;
 
+import com.company.chess_online_bakend_api.data.command.ChatMessageCommand;
 import com.company.chess_online_bakend_api.data.command.CountCommand;
 import com.company.chess_online_bakend_api.service.ChatMessageService;
 import io.swagger.annotations.ApiOperation;
@@ -18,6 +19,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -31,12 +34,24 @@ public class ChatMessageController {
     }
 
     @ApiOperation(value = "Get total message count for room.",
-            notes = "Returns 404 NOT FOUND if room does not exist")
+            notes = "Returns 404 NOT FOUND if room does not exist.")
     @GetMapping({RoomController.BASE_URL + "{id}/message/count", RoomController.BASE_URL + "{id}/message/count/"})
     @ResponseStatus(HttpStatus.OK)
     public CountCommand getMessageCount(@PathVariable Long id) {
         log.debug("New request: GET " + RoomController.BASE_URL + id + "/message/count");
 
         return CountCommand.builder().count(chatMessageService.getMessageCountForRoom(id)).build();
+    }
+
+    @ApiOperation(value = "Get message page for room.",
+            notes = "Returns 404 NOt FOUND if room does not exist. \n " +
+                    "10 messages per page.")
+    @GetMapping({RoomController.BASE_URL + "{id}/message/page/{page}",
+            RoomController.BASE_URL + "{id}/message/page/{page}/"})
+    @ResponseStatus(HttpStatus.OK)
+    public List<ChatMessageCommand> getMessagePageForRoom(@PathVariable Long id, @PathVariable int page) {
+        log.debug("New request: Get " + RoomController.BASE_URL + id + "/message/page/" + page);
+
+        return chatMessageService.getMessagePageForRoom(id, page);
     }
 }
--- src/test/java/com/company/chess_online_bakend_api/controller/chatMessage/ChatMessageControllerTest.java
+++ src/test/java/com/company/chess_online_bakend_api/controller/chatMessage/ChatMessageControllerTest.java
@@ -2,20 +2,13 @@
  * 7/26/19 8:09 PM. Created by Bartosz Kruba.
  */
 
-/*
- * 7/26/19 8:01 PM. Created by Bartosz Kruba.
- */
-
-/*
- * 7/26/19 8:01 PM. Created by Bartosz Kruba.
- */
-
 package com.company.chess_online_bakend_api.controller.chatMessage;
 
 import com.company.chess_online_bakend_api.controller.AbstractRestControllerTest;
 import com.company.chess_online_bakend_api.controller.ChatMessageController;
 import com.company.chess_online_bakend_api.controller.ExceptionAdviceController;
 import com.company.chess_online_bakend_api.controller.RoomController;
+import com.company.chess_online_bakend_api.data.command.ChatMessageCommand;
 import com.company.chess_online_bakend_api.exception.RoomNotFoundException;
 import com.company.chess_online_bakend_api.service.jpa.ChatMessageServiceJpaImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -27,7 +20,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -80,6 +77,42 @@ class ChatMessageControllerTest extends AbstractRestControllerTest {
                 .andExpect(jsonPath("$.count", equalTo(10)));
 
         verify(chatMessageService, times(1)).getMessageCountForRoom(roomId);
+        verifyNoMoreInteractions(chatMessageService);
+    }
+
+    @Test
+    void getMessagePageInvalidRoomId() throws Exception {
+        Long roomId = 1L;
+        int page = 0;
+
+        when(chatMessageService.getMessagePageForRoom(roomId, page)).thenThrow(RoomNotFoundException.class);
+
+        mockMvc.perform(get(RoomController.BASE_URL + roomId + "/message/page/" + page)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.status", equalTo(404)));
+
+        verify(chatMessageService, times(1)).getMessagePageForRoom(roomId, page);
+        verifyNoMoreInteractions(chatMessageService);
+    }
+
+    @Test
+    void getMessagePage() throws Exception {
+        Long roomId = 1L;
+        int page = 0;
+
+        List<ChatMessageCommand> chatMessageCommandList = Arrays.asList(
+                ChatMessageCommand.builder().id(1L).build(),
+                ChatMessageCommand.builder().id(2L).build());
+
+        when(chatMessageService.getMessagePageForRoom(roomId, page)).thenReturn(chatMessageCommandList);
+
+        mockMvc.perform(get(RoomController.BASE_URL + roomId + "/message/page/" + page)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+
+        verify(chatMessageService, times(1)).getMessagePageForRoom(roomId, page);
         verifyNoMoreInteractions(chatMessageService);
     }
 }
